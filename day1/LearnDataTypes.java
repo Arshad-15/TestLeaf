5c371ae9b495e61a95e1eaa3f02f0ead89d5b775
Arshad-15 <137628590+Arshad-15@users.noreply.github.com>
2023-06-24 20:17:26 +0530

    Add files via upload
    Week1.Day1

---
+++ day1/LearnDataTypes.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int age = 29;
+    boolean NewClass = true;
+    String  institue = "TestLeaf";
+    long phoneNumber =  9600770896L;
+    float decimal = 24.5f;
+    
+    System.out.println(age +"\n "+NewClass+"\n "+institue+"\n"+phoneNumber+"\n"+decimal);
+	}
+
+}
